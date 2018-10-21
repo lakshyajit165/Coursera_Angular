383e05cf8fe03584ff36f55169ae43ff2cbba063
lakshyajit165 <lakshyajit165@gmail.com>
2018-10-21 18:28:45 +0530

    Routing Fix

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { MenuComponent } from '../menu/menu.component';
 import { DishdetailComponent } from '../dishdetail/dishdetail.component';
-import { HomeComponent } from '../home/home.component';
+
+
 import { AboutComponent } from '../about/about.component';
+import { HomeComponent } from '../home/home.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,21 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
