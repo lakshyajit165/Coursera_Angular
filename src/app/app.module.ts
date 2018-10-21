0090204d5fe28e331e4f59c6af274179837aee8d
lakshyajit165 <lakshyajit165@gmail.com>
2018-10-21 19:37:36 +0530

    SPAs Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
@@ -46,7 +48,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule
   ],
   providers: [
-      DishService
+      DishService,
+      PromotionService
   ],
   bootstrap: [AppComponent]
 })
