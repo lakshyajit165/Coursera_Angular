3686bb2671ac5e6a6ea93095c957471e5089e2b3
lakshyajit165 <lakshyajit165@gmail.com>
2018-10-23 01:09:01 +0530

    SPAs Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,6 +11,7 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: 'dishDetail/:id', component: DishdetailComponent},
   { path: 'contactus', component: ContactComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
